a49e940a7125eaff03205cd25be1623d6625aabc
EdgarMC23 <edgarmilan58@gmail.com>
2025-08-04 14:34:12 -0700

    upgrade

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Proyecto_7
+
+Este proyecto tiene una aplicacion web en donde podemos ver dos botones que crean graficas diferentes que analizan el conjunto de datos de anuncios de venta de coches, que son:
+
+* Boton de creacion de histograma.
+* Boton de creacion de grafico de dispersión.
---
+++ app.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+st.header("Anuncios de venta de coches")  # Encabezado
+hist_button = st.button('Construir histograma')  # Botón de histograma
+# Botón de grafico de dispersion
+disp_button = st.button("Construir gráfico de dispersión")
+
+if hist_button:
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig_hist = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_hist, use_container_width=True)
+
+if disp_button:
+    st.write("Creación de grafico de dispersión para el conjunto de datos de anuncios de venta de coches")
+
+    # crear grafico de dispersión
+    fig_disp = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig_disp, use_container_width=True)
